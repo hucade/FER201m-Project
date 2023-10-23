a91c3c9ecead2d2d0c19fc2d41dfca26d9a795fb
hucade <quan25102001@gmail.com>
2023-10-23 12:32:10 +0700

    Job details

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'
-import { Container, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 import { Header, JobDetail, JobListing, About, Contact, HomePage, PostJob, ApplyJobs, Footer, Login, Register } from "./View/Component/index"
 
 function App() {
   return (
     <Container fluid>
-      <Header></Header>
-      <Row>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/JobDetail" element={<JobDetail />} />
-          <Route path="/JobListing" element={<JobListing />} />
-          <Route path="/postJob" element={<PostJob />} />
-          <Route path="/applyjob" element={<ApplyJobs />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Row>
-      <Footer></Footer>
+      <Col>
+        <Header></Header>
+        <Row>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/jobdetail" element={<JobDetail />} />
+            <Route path="/JobListing" element={<JobListing />} />
+            <Route path="/postJob" element={<PostJob />} />
+            <Route path="/applyjob" element={<ApplyJobs />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Row>
+        <Footer></Footer>
+      </Col>
     </Container>
   );
 }
